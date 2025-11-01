737f3417e4ce1f1117cc4994907dbe53682745ed
MinDutch03 <nguyenminhduc890@gmail.com>
2025-11-01 17:35:17 +0700

    refactor code

--- src/config/db.tsx
+++ src/config/db.tsx
@@ -1,10 +1,7 @@
 import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
-const { DATABASE_URL } = process.env;
-if (!DATABASE_URL) {
-	throw new Error('DATABASE_URL is not set. Please configure it in your environment.');
-}
+const DATABASE_URL = process.env.DATABASE_URL!;
 
 const sql = neon(DATABASE_URL);
 export const db = drizzle({ client: sql });
