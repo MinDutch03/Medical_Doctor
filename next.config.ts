702faad5d34b8b53711da7e04306a6c0ebbf7f8f
MinDutch03 <nguyenminhduc890@gmail.com>
2025-11-01 17:43:20 +0700

    update config

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
       { source: "/sign-up", destination: "/auth/sign-up" },
     ];
   },
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+    RECEIVED_GMAIL: process.env.RECEIVED_GMAIL,
+    GOOGLE_GEMINI_API_KEY: process.env.GOOGLE_GEMINI_API_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    DOCTOR_PHONE_NUMBER: process.env.DOCTOR_PHONE_NUMBER,
+  },
 };
 
 export default nextConfig;
